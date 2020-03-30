31d5f5610baba3d6ad4c9f20f9ce263cae4a7efe
Marcello Barile <marcello@pop-os.localdomain>
2020-03-30 22:10:45 +0200

    moved list items under components

--- frontend/src/app/components/items/crag-item.component.ts
+++ frontend/src/app/components/items/crag-item.component.ts
@@ -10,7 +10,7 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'wci-crag-list-item',
   templateUrl: 'crag-item.component.html',
-  styleUrls: ['../common.scss'],
+  styleUrls: ['crag-item.component.scss'],
 })
 export class CragListItemComponent extends BaseListItemComponent implements OnInit {
   @Input() data: Crag;
