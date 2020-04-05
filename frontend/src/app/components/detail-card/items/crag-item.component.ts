995e8e0f52c46898d2223338d6b3e7354f3eaf39
Marcello Barile <marcello.barile@gmail.com>
2020-04-05 11:52:01 +0200

    moved common code to the base card comp.

--- frontend/src/app/components/detail-card/items/crag-item.component.ts
+++ frontend/src/app/components/detail-card/items/crag-item.component.ts
@@ -1,14 +1,11 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { GeoLocation } from 'src/app/classes/geolocation.class';
-import { ForecastResult, NearbyResult } from 'src/app/graphql/queries';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { ForecastResult } from 'src/app/graphql/queries';
 import { Crag } from 'src/app/interfaces/graphql/crag.type';
 import { CurrentWeather } from 'src/app/interfaces/graphql/currentweather.type';
 import { Forecast } from 'src/app/interfaces/graphql/forecast.type';
-import { Poi } from 'src/app/utils/Poi';
-import { environment } from 'src/environments/environment';
-import { BaseCardItemComponent } from './base-item.component';
-import { Router } from '@angular/router';
 import { WciApiService } from 'src/app/services/wciApi.service';
+import { BaseCardItemComponent } from './base-item.component';
 
 @Component({
   selector: 'wci-crag-card-item',
@@ -16,12 +13,6 @@ import { WciApiService } from 'src/app/services/wciApi.service';
   styleUrls: ['crag-item.component.scss'],
 })
 export class CragCardItemComponent extends BaseCardItemComponent implements OnChanges {
-  @Input() data: Crag;
-  @Input() currentLocation: GeoLocation;
-
-  mapboxToken = environment.mapbox.token;
-  staticMapSrc = '';
-
   hasForecast = false;
   forecastClass = 'clear-day';
   currentWeather: CurrentWeather;
@@ -29,21 +20,18 @@ export class CragCardItemComponent extends BaseCardItemComponent implements OnCh
   tomorrowForecast: Forecast;
   forecasts: Forecast[];
 
-  nearbyPois: Poi[] = [];
-
-  private staticMapSizes = [303, 360];
+  data: Crag;
 
   constructor(protected router: Router, protected api: WciApiService) {
     super(router, api);
+    this.data = super.data;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes.data && changes.data.currentValue && changes.data.currentValue !== changes.data.previousValue) {
-      // tslint:disable-next-line: max-line-length
-      this.staticMapSrc = `https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/static/${this.data.coords.lng},${this.data.coords.lat},10.19,0/${this.staticMapSizes[0]}x${this.staticMapSizes[1]}@2x?access_token=${this.mapboxToken}`;
+    super.ngOnChanges(changes);
 
+    if (changes.data && changes.data.currentValue && changes.data.currentValue !== changes.data.previousValue) {
       this.getForecast();
-      this.getNearby();
     }
   }
 
@@ -74,29 +62,6 @@ export class CragCardItemComponent extends BaseCardItemComponent implements OnCh
               this.forecastClass = 'clear-day';
             }
           }
-          sub$.unsubscribe();
-        });
-    }
-  }
-
-  /**
-   *
-   */
-  getNearby(): void {
-    if (this.data.coords && this.data.coords.lat && this.data.coords.lng) {
-      const sub$ = this.api
-        .getNearby({ lat: this.data.coords.lat, lng: this.data.coords.lng, distance: 80, start: 0, end: 4 })
-        .subscribe((res: NearbyResult) => {
-          if (!res.errors) {
-            // TODO: The slice is needed because there is a bug on the BE
-            // where the limits seems to be discarded for the nearby query
-            this.nearbyPois = [
-              ...res.data.nearby.crags.slice(0, 4),
-              ...res.data.nearby.hikes.slice(0, 4),
-              ...res.data.nearby.shelters.slice(0, 4),
-            ].filter((poi: Poi) => poi.slug !== this.data.slug);
-          }
-
           sub$.unsubscribe();
         });
     }
